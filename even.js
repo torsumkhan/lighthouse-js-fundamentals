810946c2e7ae4f29a38365c7ce9241d9620240c8
torsumkhan <torsum.khan@gmail.com>
2022-01-02 19:41:49 +0000

    more functions

---
+++ even.js
@@ -0,0 +1,19 @@
+/*const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elev (num) {
+  return num % 2 === 0;
+}enIsEven);
+*/
+
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
