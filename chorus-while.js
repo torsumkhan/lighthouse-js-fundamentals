6492aaab21ba25fe0961e75df2b6fa2947ec75a0
torsumkhan <torsum.khan@gmail.com>
2022-01-02 06:22:01 +0000

    used while loops

---
+++ chorus-while.js
@@ -0,0 +1,14 @@
+const chorus = "Let's dance!";
+
+let repeat = 0;
+
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("change key")
+  }
+;
+  console.log(chorus)
+  repeat ++;
+}
+
+console.log("Untill the sum comes up!")
