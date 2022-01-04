c4e014a17029e1a47b304f8498ba854aed89d8b0
torsumkhan <torsum.khan@gmail.com>
2022-01-04 15:54:25 +0000

    while loops

--- packing-list.js
+++ packing-list.js
@@ -2,6 +2,9 @@ const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glas
 
 console.log("kitchen stuff to pack: ")
 
-for (let i = 0; i < packingList.length; i ++) {
+let i = 0
+
+while (i < packingList.length){
   console.log(packingList[i])
+  i ++;
 }
