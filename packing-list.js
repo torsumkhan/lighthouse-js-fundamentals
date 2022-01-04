848abf71bea5003af3cd35a240aebadf7adecbc8
torsumkhan <torsum.khan@gmail.com>
2022-01-04 10:48:31 -0500

    new file

---
+++ packing-list.js
@@ -0,0 +1,7 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("kitchen stuff to pack: ")
+
+for (let i = 0; i < packingList.length; i ++) {
+  console.log(packingList[i])
+}
