7bcfcb919b56734b92478fc18fc96e7c00d3762e
torsumkhan <torsum.khan@gmail.com>
2022-01-04 16:17:00 +0000

    bannana bread

---
+++ ingredients.js
@@ -0,0 +1,19 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+//while loop
+/*let i = 0
+
+while (i < ingredients.length) {
+  console.log(ingredients[i])
+  i ++; 
+}
+
+//for loop
+
+for(i = 0; i < ingredients.length - 1; i --){
+  console.log(ingredients[i])
+}*/
+
+for (i = ingredients.length -1; i>=0; i--){
+  console.log(ingredients[i])
+}
